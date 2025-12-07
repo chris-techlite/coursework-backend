519768c72e325ef81b6caa788f80061098751829
chris-techlite <chilurunkachineduokoro@gmail.com>
2025-12-07 17:25:05 +0400

    fixed my get request

--- server.js
+++ server.js
@@ -1,41 +1,83 @@
 const express = require('express');
-var filesystem = require('fs');
-var path = require('path');
 const app = express();
+const fs = require('fs');
+const path = require('path');
+const MongoClient = require('mongodb').MongoClient;
 
 app.use(express.json());
 
+// CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
-    
-});
-const mongoclient = require('mongodb').MongoClient;
-
-let db;
-mongoclient.connect('mongodb+srv://Christain_CO:monday@cluster0.jvuabsa.mongodb.net/', (err, client) => {
-    db = client.db('coursework')
-});
-app.get('/', (req, res)=>{
-
-
-
 });
 
-app.use(function(req, res, next){
-    var filepath = path.join(__dirname, 'static', req.url);
-    filesystem.stat(filepath, function(err, fileinfo){
-        if(err){
-            next();
-            return;
-        }
-        if(fileinfo.isFile()){
-            res.sendFile(filepath);
-        } else {
-            next();
-        }
+// Logger middleware
+app.use((req, res, next) => {
+    console.log(req.method, req.url);
+    next();
+});
+
+// Static files
+app.use((req, res, next) => {
+    const filepath = path.join(__dirname, 'static', req.url);
+    fs.stat(filepath, (err, fileinfo) => {
+        if (!err && fileinfo.isFile()) res.sendFile(filepath);
+        else next();
     });
+});
+
+// Connect to MongoDB
+let db;
+MongoClient.connect('mongodb+srv://Christain_CO:monday@cluster0.jvuabsa.mongodb.net/', (err, client) => {
+    if (err) throw err;
+    db = client.db('coursework');
+    console.log("MongoDB connected");
+});
+
+// GET all lessons
+app.get('/lessons', (req, res) => {
+    db.collection('lessons').find({}).toArray((err, results) => {
+        if (err) res.status(500).send(err);
+        else res.json(results);
+    });
+});
+
+app.post('/orders', (req, res) => {
+    const order = req.body;
+    db.collection('orders').insertOne(order, (err, result) => {
+        if (err) res.status(500).send(err);
+        else res.json({ message: "Order saved" });
+    });
+});
+
+app.put('/lessons/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const update = req.body;
+    db.collection('lessons').updateOne({ id: id }, { $set: update }, (err, result) => {
+        if (err) res.status(500).send(err);
+        else res.json({ message: "Lesson updated" });
+    });
+});
+
+app.get('/search', (req, res) => {
+    const keyword = req.query.keyword || '';
+    db.collection('lessons').find({
+        $or: [
+            { title: { $regex: keyword, $options: 'i' } },
+            { description: { $regex: keyword, $options: 'i' } },
+            { location: { $regex: keyword, $options: 'i' } },
+            { price: { $regex: keyword, $options: 'i' } },
+            { availableInventory: { $regex: keyword, $options: 'i' } }
+        ]
+    }).toArray((err, results) => {
+        if (err) res.status(500).send(err);
+        else res.json(results);
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
 });
